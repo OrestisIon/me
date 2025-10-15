4c429f54c4ee2565399f2cd2c6c4ed2441f8fdef
Orestis <orestisj@gmail.com>
2025-10-15 23:24:34 +0100

    Ready for deployment

---
+++ dist/content/ProjectsTags.md
@@ -0,0 +1,7 @@
+Java
+Javascript
+C Programming Language
+CSS3
+Cyber Security
+HTML5
+Flask
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Serve from root path (change to '/subpath/' if deploying to subdirectory)
   esbuild: {
     loader: 'jsx',
     include: /src\/.*\.jsx?$/,
@@ -21,8 +22,16 @@ export default defineConfig({
     open: true
   },
   build: {
-    outDir: 'build',
-    sourcemap: true
+    outDir: 'dist', // Changed from 'build' to 'dist' (AWS Amplify default)
+    sourcemap: false, // Disabled for production (reduces bundle size)
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          chakra: ['@chakra-ui/react', '@emotion/react', '@emotion/styled']
+        }
+      }
+    }
   },
   define: {
     'process.env': {}
